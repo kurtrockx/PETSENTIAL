cfbbfb88be4ee94f04cfadedf05fc5a94557dbe1
derpfear <kurtdebelen431@gmail.com>
2024-12-12 02:40:56 +0800

    added dashboard

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,10 +1,6 @@
 import UserModel from "../../src/js/model/userModel";
-import phLogo from "../../src/assets/images/phLogo.png";
+import phLogo from "../../src/assets/images/logo.png";
 import phLogo2 from "../../src/assets/images/phLogo2.png";
-import burgerIcon from "../../src/assets/svg/burger.svg";
-import xIcon from "../../src/assets/svg/x.svg";
-import cartIcon from "../../src/assets/images/cart.png";
-import userIcon from "../../src/assets/images/user.png";
 
 const checkAdmin = () => {
   if (!UserModel.currentUser) return;
@@ -18,31 +14,6 @@ const checkAdmin = () => {
   }
 };
 
-const burgerMenu = (
-  navlinksBackground,
-  navlinksContainer,
-  burgerButton,
-  burgerExitButton
-) => {
-  const burgerMenuOpen = () => {
-    navlinksBackground.classList.remove("hidden");
-    navlinksContainer.classList.remove("burger-translate");
-  };
-  const burgerMenuClose = () => {
-    navlinksContainer.classList.add("burger-translate");
-    navlinksBackground.classList.add("hidden");
-  };
-  burgerButton.addEventListener("click", burgerMenuOpen);
-  [navlinksBackground, burgerExitButton].forEach((el) =>
-    el.addEventListener("click", burgerMenuClose)
-  );
-  window.addEventListener("keydown", (e) => {
-    if (e.key === "Escape") {
-      burgerMenuClose();
-    }
-  });
-};
-
 const logout = (logoutButton) => {
   logoutButton.addEventListener("click", UserModel.logoutUser);
 };
@@ -53,60 +24,21 @@ const insertNavbar = () => {
       <div class="navbar">
         <a href="index.html" class="navlogo">
           <img src="${phLogo}" />
-          <p class="pormahub-logo-text">
-            PORMA <span id="hub-logo-text">HUB</span>
-          </p>
+          <p class="">PETSENTIAL</p>
         </a>
-        <div class="nav-burger">
-          <img src="${burgerIcon}" />
-        </div>
-        <div class="navlinks-background hidden"></div>
-        <div class="navlinks-container burger-translate">
-          <img src="${xIcon}" class="burger-exit" />
-          <div class="navlinks-title">Porma<span id="hub-text">hub</span></div>
-          
-          <div class="navlink-icon-container">
-            <a href="${
-              UserModel.currentUser ? "cart.html" : "login.html"
-            }" class="navlink-icon">
-              SHOPPING CART
-              <img src="${cartIcon}" />
-            </a>
-            <a href="${
-              UserModel.currentUser ? "profile.html" : "login.html"
-            }" class="navlink-icon">
-            ${UserModel.currentUser?.username ?? "SIGN IN"}
-              <img src="${userIcon}" />
-            </a>
+        <div class="navlink-logo-pet">
+            <a href="index.html" class="navlink">HOME</a>
+            <a href="store.html" class="navlink">STORE</a>
+            <a href="cart.html" class="navlink">CART  </a>
+            <a href="about.html" class="navlink">ABOUT US</a>
+            <a href="index.html" class="navlink navlink-logout">LOG OUT</a>
           </div>
-
-          <a href="index.html" class="navlink">HOME</a>
-          <a href="store.html" class="navlink">STORE</a>
-          <a href="about.html" class="navlink">ABOUT US</a>
-          <a href="index.html" class="navlink navlink-logout">LOG OUT</a>
-
-          <img src="${phLogo2}" class="navlink-logo" />
-
-        </div>
       </div>
     </nav>
 `;
   document.body.insertAdjacentHTML("afterbegin", html);
-  //Navigation
-  const navlinksBackground = document.querySelector(".navlinks-background");
-  const navlinksContainer = document.querySelector(".navlinks-container");
-  //Burger
-  const burgerButton = document.querySelector(".nav-burger");
-  const burgerExitButton = document.querySelector(".burger-exit");
-  //Logout
   const logoutButton = document.querySelector(".navlink-logout");
 
-  burgerMenu(
-    navlinksBackground,
-    navlinksContainer,
-    burgerButton,
-    burgerExitButton
-  );
   logout(logoutButton);
 };
 
